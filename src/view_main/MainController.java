e6dc2eedf6bc11071cd43a384114e15591fb6b2e
jessechen09 <54601077+jessechen09@users.noreply.github.com>
2021-04-12 21:33:09 -0700

    updated initialize, removed default constructor

--- src/view_main/MainController.java
+++ src/view_main/MainController.java
@@ -7,33 +7,46 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.PasswordManagerModel;
+import user.InternetAccount;
 import user.User;
 
 import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainController {
     private PasswordManagerModel model;
     private Stage stage;
     private User user;
 
-//    @FXML
-//    private Button addNewPasswordButton;
+    @FXML
+    private Button addNewPasswordButton;
 
     @FXML
     private VBox passwordsVBox;
 
-    // FXMl file can only call a default constructor
-    public MainController() {
+    /**
+     * FXMl file can only call a default constructor, but we can't do much inside it, so let's just
+     * remove it. For example, we want to transfer the PasswordManagerModel object from the LoginController class to
+     * the MainController class. Instead, we use a method that is called by the LoginController class that will do
+     * this job, along many others.
+     *
+     * @param model
+     */
+    public void initialize(PasswordManagerModel model) {
+        this.model = model;
+        this.user = model.getCurrentUser();
+        System.out.println("Model transferred from login window to main window");
+        System.out.println("This account has these domains stored:");
+        HashMap<String, InternetAccount> internetAccounts = user.getInternetAccounts();
+        for (String domain : internetAccounts.keySet()) {
+            System.out.println(domain);
+            //display passwords in mainWindow
+        }
     }
 
-    @FXML
-    public void initialize() {
-        System.out.println("pwbbox: " + passwordsVBox);
+    public void addNewPasswordButtonAction() {
+        System.out.println("Adding new password...");
+        //launch addNewPasswordWindow
     }
-    public void setModel(PasswordManagerModel model){
-        this.model = model;
-    }
-//    public void addNewPasswordButtonAction(ActionEvent event) {
-//        System.out.println("Adding new password...");
-//    }
 }

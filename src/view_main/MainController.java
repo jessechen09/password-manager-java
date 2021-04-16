ae0c2b25ae03921ef0a747c9f29dbc6be3374c74
jessechen09 <54601077+jessechen09@users.noreply.github.com>
2021-04-16 00:33:20 -0700

    added logout button and hboxes

--- src/view_main/MainController.java
+++ src/view_main/MainController.java
@@ -1,16 +1,22 @@
 package view_main;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import launcher.PasswordManager;
+import launcher.PasswordManagerLauncher;
 import model.PasswordManagerModel;
 import user.InternetAccount;
 import user.User;
 
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,12 +24,16 @@ public class MainController {
     private PasswordManagerModel model;
     private Stage stage;
     private User user;
+    private Map<String,HBox> hboxMap;
 
     @FXML
     private Button addNewPasswordButton;
 
     @FXML
     private VBox passwordsVBox;
+
+    @FXML
+    private Button logoutButton;
 
     /**
      * FXMl file can only call a default constructor, but we can't do much inside it, so let's just
@@ -33,20 +43,30 @@ public class MainController {
      *
      * @param model
      */
-    public void initialize(PasswordManagerModel model) {
-        this.model = model;
-        this.user = model.getCurrentUser();
+    public void initialize(PasswordManagerModel model) throws IOException {
         System.out.println("Model transferred from login window to main window");
         System.out.println("This account has these domains stored:");
-        HashMap<String, InternetAccount> internetAccounts = user.getInternetAccounts();
-        for (String domain : internetAccounts.keySet()) {
+
+        this.model = model;
+        this.user = model.getCurrentUser();
+        hboxMap = new HashMap<String, HBox>();
+        for (String domain : user.getInternetAccounts().keySet()) {
             System.out.println(domain);
-            //display passwords in mainWindow
+            HBox userHBox = FXMLLoader.load(getClass().getResource("UserHBoxView.fxml"));
+            passwordsVBox.getChildren().add(userHBox);
+            hboxMap.put(domain,userHBox);
         }
     }
 
     public void addNewPasswordButtonAction() {
         System.out.println("Adding new password...");
+
         //launch addNewPasswordWindow
+    }
+
+    public void logoutButtonAction() throws Exception {
+        logoutButton.getScene().getWindow().hide();
+        PasswordManagerLauncher pml = new PasswordManagerLauncher();
+        pml.start(new Stage());
     }
 }

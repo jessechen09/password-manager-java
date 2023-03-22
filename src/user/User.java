64e92bcaf8383181f36d6b199bf7f51aa9b6cc97
Hugo Pereira <hugogspereira@gmail.com>
2023-03-22 02:14:52 +0000

    changed the extension of the files used to save data

--- src/user/User.java
+++ src/user/User.java
@@ -1,81 +1,91 @@
 package user;
 
 import model.PasswordManagerModel;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
-/**
- * The User object holds all account info, including the account info for
- * logging into the password manager and all Internet account info.
- *
- * @author Jesse Chen
- */
-
 public class User {
     private Account account;
     private HashMap<String, InternetAccount> internetAccounts;
 
-    // take account as parameter because maybe we want to add more info to account in future
     public User(Account account) {
         this.account = account;
         this.internetAccounts = new HashMap<String, InternetAccount>();
 
         // load in all of user's internet account info
         try {
-            Scanner scanner = new Scanner(new File(PasswordManagerModel.DATA_DIRECTORY + account.getUserName() +
-                    ".txt"));
-            String domain;
-            String username;
-            String password;
-            while (scanner.hasNextLine()) {
-                domain = scanner.next();
-                username = scanner.next();
-                password = scanner.next();
-                internetAccounts.put(domain, new InternetAccount(domain, username, password));
+            File file = new File(PasswordManagerModel.DATA_DIRECTORY + account.getUserName() + ".json");
+            if (file.exists()) {
+                Scanner scanner = new Scanner(file);
+                StringBuilder jsonStr = new StringBuilder();
+                while (scanner.hasNextLine()) {
+                    jsonStr.append(scanner.nextLine());
+                }
+                JSONArray jsonArray = new JSONArray(jsonStr.toString());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String domain = jsonObject.getString("domain");
+                    String username = jsonObject.getString("username");
+                    String password = jsonObject.getString("password");
+                    internetAccounts.put(domain, new InternetAccount(domain, username, password));
+                }
             }
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     * @param domain
-     * @return Returns the InternetAccount corresponding to the input domain.
-     */
-    public InternetAccount getInternetAccount(String domain) { return internetAccounts.get(domain); }
+    public InternetAccount getInternetAccount(String domain) {
+        return internetAccounts.get(domain);
+    }
 
-    /**
-     * @return Returns this User's Acccount.
-     */
-    public Account getAccount() { return account; }
+    public Account getAccount() {
+        return account;
+    }
 
-    /**
-     * @return Returns a HashMap of InternetAccounts.
-     */
-    public HashMap<String, InternetAccount> getInternetAccounts() { return internetAccounts; }
+    public HashMap<String, InternetAccount> getInternetAccounts() {
+        return internetAccounts;
+    }
 
-    /**
-     * Adds an InternetAccount to the HashMap and writes the domain, username, and password in the corresponding .txt
-     * file.
-     *
-     * @param internetAccount
-     */
     public void addInternetAccount(InternetAccount internetAccount) {
         try {
             String domain = internetAccount.getDomain();
             String username = internetAccount.getUserName();
             String password = internetAccount.getPassword();
-            File file = new File(PasswordManagerModel.DATA_DIRECTORY + account.getUserName() + ".txt");
-            FileWriter writer = new FileWriter(file, true);
-            writer.write("\n" + domain + " " + username + " " + password);
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("domain", domain);
+            jsonObject.put("username", username);
+            jsonObject.put("password", password);
+
+            File file = new File(PasswordManagerModel.DATA_DIRECTORY + account.getUserName() + ".json");
+
+            // Read in existing JSON array from file
+            JSONArray jsonArray;
+            if (file.exists() && file.length() > 0) {
+                Scanner scanner = new Scanner(file);
+                String jsonStr = scanner.useDelimiter("\\Z").next();
+                jsonArray = new JSONArray(jsonStr);
+            } else {
+                jsonArray = new JSONArray();
+            }
+
+            // Add new object to array
+            jsonArray.put(jsonObject);
+
+            // Write updated array back to file
+            FileWriter writer = new FileWriter(file);
+            writer.write(jsonArray.toString());
             writer.close();
+
             internetAccounts.put(domain, new InternetAccount(domain, username, password));
-        } catch (IOException e) {
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
     }
